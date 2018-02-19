ce6a9acca41630755350edc278d9d0d75218a37c
vksantoshsunny <vksantoshsunny@gmail.com>
2018-02-19 20:17:08 +1300

    Routers Link

--- src/app.js
+++ src/app.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+
+const ExpenseDashboard = () => (
+    <div>
+    This is an Expense Dashboard <Link to="/Create">Go to Create</Link>
+    </div>
+)
+
+const ExpenseCreate = () => (
+    <div>
+    This is an Expense Create <Link to="/">Go to Home</Link>
+    </div>
+)
+
+const NotFoundPage = () => (
+    <div>
+    404! <Link to="/">Go to Home</Link>
+    </div>
+)
+
+const Header = () => (
+    <h1>
+    This is a Header
+    </h1>
+)
+
+const routes = (
+    <BrowserRouter>
+    <div>
+    <Header/>
+    <Switch>
+    <Route path="/" component={ExpenseDashboard} exact={true}/>
+    <Route path="/Create" component={ExpenseCreate}  />
+    <Route  component={NotFoundPage}  />
+    </Switch>
+    </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
