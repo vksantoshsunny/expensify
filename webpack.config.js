3f8367869cca719ee9785126bd8572369fb18eba
vksantoshsunny <vksantoshsunny@gmail.com>
2018-02-19 20:56:59 +1300

    portfolio

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,6 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback:true
+    historyApiFallback: true
   }
 };
