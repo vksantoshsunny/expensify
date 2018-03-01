6736585bcd318a142e60189c153e31723c3573bd
vksantoshsunny <vksantoshsunny@gmail.com>
2018-03-01 19:21:48 +1300

    multiple reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,38 @@
 import {createStore, combineReducers} from 'redux'
 
-const demostate = {
-    expenses: [{
 
-    }],
-    filters:{
-        
-    }
+const expenseReducerDefaults = {
+    expenses: []
 }
+
+const expenseReducer = (state = expenseReducerDefaults, action) => {
+    switch(action.type){
+        default:
+        return state;
+    }
+ 
+}
+
+
+const filtersReducerDefaults = {
+    id:"0",
+    date:"",
+    description: "some description"
+}
+const filtersReducer = (state = filtersReducerDefaults, action) => {
+    switch(action.type){
+        default:
+        return state;
+    }
+ 
+}
+
+
+
+const store = createStore(combineReducers({
+    expenses: expenseReducer,
+    filters: filtersReducer
+}));
+
+
+console.log(store.getState())
